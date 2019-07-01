deaf6850d2770d182891edec9b4a340aa4820d6f
Milos Kaurin <milos.kaurin@gmail.com>
2019-07-02 00:18:35 +0100

    More concurrecny - more performance (#24)
    As our code goes through profiles / regions / services and goes into
    resource-level processing it has some text processing to do (at the moment).
    
    Before this change, text processing would block each new resource coming
    into the channel, but no longer. We're not processing every resource in it's
    goroutine as it comes into the resource processing channel.
    
    Bonus: Equalized ec2-instance, ec2-address and rds-cluster resource
    searches so the code is similar.
    
    Code in these functions does look quite duplicated, but I'm not sure if I'll be 
    able to break this down any better.

--- lib/searchec2/instances.go
+++ lib/searchec2/instances.go
@@ -3,6 +3,7 @@ package searchec2
 import (
 	"context"
 	"strings"
+	"sync"
 
 	"github.com/Kaurin/megantory/lib/common"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -11,27 +12,35 @@ import (
 
 // searchInstances searches single AWS EC2 region
 func searchInstances(ec2i ec2Input) {
+	wg := sync.WaitGroup{}
 	service := "ec2"
 	resourceType := "ec2-instance"
 	bcr := common.BreadCrumbs(ec2i.profile, ec2i.region, service, resourceType)
 	cInstances := make(chan ec2.Instance)
 	go describeInstances(ec2i.client, cInstances)
-	for instance := range cInstances { // Blocked until describeInstances closes chan
-		instanceLower := strings.ToLower(instance.String())
-		searchStrLower := strings.ToLower(ec2i.searchStr)
-		if strings.Contains(instanceLower, searchStrLower) {
-			result := common.Result{
-				Account:      ec2i.profile,
-				Region:       ec2i.region,
-				Service:      service,
-				ResourceType: resourceType,
-				ResourceID:   *instance.InstanceId,
-				ResourceJSON: instance.String(),
+	for instanceL := range cInstances {
+		instance := instanceL
+		wg.Add(1)
+		go func() {
+			instanceLower := strings.ToLower(instance.String())
+			searchStrLower := strings.ToLower(ec2i.searchStr)
+			if strings.Contains(instanceLower, searchStrLower) {
+				result := common.Result{
+					Account:      ec2i.profile,
+					Region:       ec2i.region,
+					Service:      service,
+					ResourceType: resourceType,
+					ResourceID:   *instance.InstanceId,
+					ResourceJSON: instance.String(),
+				}
+				log.Debugf("%s: Matched an %s, sending back to the results channel.", bcr, resourceType)
+				ec2i.cResult <- result
 			}
-			log.Debugf("%s: Matched an instance, sending back to the results channel.", bcr)
-			ec2i.cResult <- result
-		}
+			wg.Done()
+		}()
+
 	}
+	wg.Wait()
 	ec2i.parentWg.Done()
 }
 
